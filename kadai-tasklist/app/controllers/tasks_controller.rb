ce92a20bfe1e8b20edf1b207efef7255f1dfcb9b
bassa <yosshi.spurs@gmail.com>
2019-04-18 10:54:31 +0000

    init

---
+++ kadai-tasklist/app/controllers/tasks_controller.rb
@@ -0,0 +1,66 @@
+class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+   
+  def index
+     @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+     @task = Task.new
+  end
+
+  def create
+      @task = current_user.tasks.build(task_params)
+
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end  
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url  
+  end
+  
+ private
+ 
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+end
