d39ae368104ccd37a338c62fd59b6be9e00dd4c5
bassa <yosshi.spurs@gmail.com>
2019-04-18 13:40:43 +0000

    user registation

--- monolist/config/routes.rb
+++ monolist/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
     root to:'toppages#index'
+    get 'signup', to: 'users#new'
+    resources :users, only: [:show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
